be9c749642b187532752288eb9421b41b25e40a3
Brandon Brown <hoobie4792@yahoo.com>
2020-08-17 11:51:49 -0400

    Configured jobs and contacts controllers

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::ContactsController < ApplicationController
+
+    before_action :authenticate, :only => [:index, :create]
+
+    def index
+        if current_user
+            render :json => current_user.contacts.as_json(:except => [:id, :user_id])
+        else
+            render :json => { message: 'Must be logged in to see contacts' }
+        end
+    end
+
+    def create
+        if current_user
+            contact = Contact.new(contact_params)
+            contact.user = current_user
+            if contact.save
+                render :json => contact.as_json(:except => [:id, :user_id]), :status => :ok
+            else
+                render :json => { message: contact.errors.full_messages.join(', ') }, :status => :bad_request
+            end
+        else
+            render :json => { message: 'Must be logged in to create a job' }
+        end
+    end
+
+    private
+
+    def contact_params
+        params.require(:contact).permit(:name, :company, :job_title, :email, :phone, :linkedin_profile)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
+            resources :contacts, :only => [:index, :show, :create, :update, :destroy]
+            resources :jobs, :only => [:index, :show, :create, :update, :destroy]
+
             post '/login', :to => 'sessions#create'
             post '/signup', :to => 'users#create'
         end
